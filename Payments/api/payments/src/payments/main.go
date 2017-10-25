69615fc191818622ad7cf6dfbb15c82cbc68e756
Aashish Subramanian <aashish.subramanian90@gmail.com>
2017-10-24 23:24:13 -0700

    main.go - Assigns a port and starts a server to listen on the port

--- Payments/api/payments/src/payments/main.go
+++ Payments/api/payments/src/payments/main.go
@@ -1,11 +1,24 @@
 package main
 
+// Import libraries
 import (
 	'os'
 )
+/*
+
+Function name: main
+Parameters: None
+Purpose: Specify server port and start listening on the specified port
+Return value: None
+
+*/
 func main(){
 	port:=os.Getenv("PORT")
 	if len(port) == 0{
 		port = "3000"
 	}
+	// Create a new server
+	server := NewServer()
+	// Start listening on specified port
+	server.Run(":" + port)
 }
