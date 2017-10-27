a58909a45abd8e9ee8f71e783e1f404011a92ec7
Niral Koradia <koradiya.niral@gmail.com>
2017-10-27 11:37:54 -0700

    Shopping Cart Session Update

--- ShoppingCart/ninja/routes/index.js
+++ ShoppingCart/ninja/routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var Product = require('../models/product');
+var Cart = require('../models/cart');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Shopping Cart' });
+router.use(function (req, res, next) {
+    res.locals.login = req.isAuthenticated();
+    next();
+});
+
+router.get('/', function (req, res, next) {
+    Product.find(function (err, docs) {
+        var productChunks = [];
+        var chunkSize = 3;
+        for (var i = 0; i < docs.length; i += chunkSize) {
+            productChunks.push(docs.slice(i, i + chunkSize));
+        }
+        res.render('shop/index', {title: 'Shopping Cart', products: productChunks});
+    });
+});
+
+router.get('/add-to-cart/:id', function (req, res, next) {
+    var productId = req.params.id;
+    var cart = new Cart(req.session.cart ? req.session.cart.items : {});
+    
+    Product.findById(productId, function (err, product) {
+        cart.add(product, product.id);
+        req.session.cart = cart;
+        res.redirect('/');
+    });
+});
+
+router.get('/shopping-cart', function (req, res, next) {
+    if (!req.session.cart) {
+        return res.render('shop/shopping-cart', {products: null});
+    }
+    var cart = new Cart(req.session.cart.items);
+    res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
 });
 
 module.exports = router;
