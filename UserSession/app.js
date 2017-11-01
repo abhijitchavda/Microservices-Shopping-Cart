62d104f41f75882f0dbf639fd175d570a5b709bf
prathmesh-parmar <31287314+prathmesh-parmar@users.noreply.github.com>
2017-10-31 19:47:52 -0700

    Sign Up functionality using mongodb replication

---
+++ UserSession/app.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var app = express();
+var port = 3000;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017,localhost:27018,localhost:27019/login2?replicaSet=prathmesh");
+var nameSchema = new mongoose.Schema({
+    Emailid: String,
+    Password: String
+});
+var User = mongoose.model("User", nameSchema);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/signup", (req, res) => {
+    var myData = new User(req.body);
+    myData.save()
+        .then(item => {
+            res.send("Name saved to database");
+        })
+        .catch(err => {
+            res.status(400).send("Unable to save to database");
+        });
+});
+
+app.listen(port, () => {
+    console.log("Server listening on port " + port);
+});
