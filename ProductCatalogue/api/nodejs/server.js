b5b6d01b7600a6cb7b419a27180e07e40a7614f9
Abhijitsinh Chavda <abhijitsinh.chavda@sjsu.edu>
2017-11-01 13:15:51 -0700

    Added apis for filtering and reading prodct details
    Updated server.js to solve CORS issue.

--- ProductCatalogue/api/nodejs/server.js
+++ ProductCatalogue/api/nodejs/server.js
@@ -7,13 +7,26 @@ var url = require('url');
 var express=require("express");
 var app=express();
 var ObjectID = require('mongodb').ObjectID
+var ip=require('ip');
 
+var path = require('path');
+app.use(express.static(path.resolve('./public')));
+app.use(function(req, res, next) {
+ res.header("Access-Control-Allow-Origin", "*");
+ res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+ next();
+});
 app.get('/mostfav', function (request, response, next) {
 			response.setHeader("Content-Type","application/json");
-			MongoClient.connect(murl, function(err, db) {                  //get famous item for home page
+			MongoClient.connect(murl, function(err, db) {  //get famous item for home page
   			assert.equal(null, err);
   			db.collection("catalogitems").find().sort({avg_ratings:-1}).limit(constantm.limitvalue).toArray(function(err, result) {
     		if (err) throw err;
+    		//console.dir('http://'+ip.address()+':3000/mostfav');
+console.dir('http://'+request.ip+':3000/mostfav');
+    		for (i=0;i<result.length;i++){
+    			result[i].img="http://"+ip.address()+":"+constantm.port+result[i].img;
+    		}
     		response.json(result);
     		response.end();
 			});
@@ -24,10 +37,13 @@ app.get('/mostfav', function (request, response, next) {
 app.get('/catagory/:catagory', function (request, response, next) {
 			var cat=request.params.catagory;
 			response.setHeader("Content-Type","application/json");
-			MongoClient.connect(murl, function(err, db) {                //get content for perticular catagory
+			MongoClient.connect(murl, function(err, db) {  //get content for perticular catagory
   			assert.equal(null, err);
   			db.collection("catalogitems").find({"catagory":cat}).toArray(function(err, result) {
     		if (err) throw err;
+    		for (i=0;i<result.length;i++){
+    			result[i].img="http://"+ip.address()+":"+constantm.port+result[i].img;
+    		}
     		response.json(result);
     		response.end();
 			});
@@ -42,12 +58,15 @@ app.get('/catagory/:catagory/sort/:variant/:type', function (request, response, 
   			var bsort = {};
   			bsort[variant] = -1;
 			response.setHeader("Content-Type","application/json");
-			MongoClient.connect(murl, function(err, db) {                 //sort based on price,avg_ratings and hightolow and lowtohigh
+			MongoClient.connect(murl, function(err, db) {  //sort based on price,avg_ratings and hightolow and lowtohigh
   			assert.equal(null, err);
   			if (request.params.type=="hightolow")
   			{
   				db.collection("catalogitems").find({"catagory":cat}).sort(bsort).toArray(function(err, result) {
     			if (err) throw err;
+    			for (i=0;i<result.length;i++){
+    			result[i].img="http://"+ip.address()+":"+constantm.port+result[i].img;
+    		}
     			response.json(result);
     			response.end();
   				});  
@@ -56,6 +75,9 @@ app.get('/catagory/:catagory/sort/:variant/:type', function (request, response, 
   			{
   				db.collection("catalogitems").find({"catagory":cat}).sort(asort).toArray(function(err, result) {
     			if (err) throw err;
+    			for (i=0;i<result.length;i++){
+    			result[i].img="http://"+ip.address()+":"+constantm.port+result[i].img;
+    		}
     			response.json(result);
     			response.end();
   				});  
@@ -64,4 +86,46 @@ app.get('/catagory/:catagory/sort/:variant/:type', function (request, response, 
 		});
 });
 
-app.listen(3000)
+app.get('/catagory/:catagory/filter/price', function (request, response, next) {
+			var cat=request.params.catagory;
+			var upperl=parseInt(request.query.ul);
+			var lowerl=parseInt(request.query.ll);
+			var query = {
+							catagory: cat,
+    						price: {$gt:lowerl,$lt:upperl}
+						};
+			response.setHeader("Content-Type","application/json");
+			MongoClient.connect(murl, function(err, db) {  //filter based on price with min and max value
+  			assert.equal(null, err);
+			db.collection("catalogitems").find(query).sort({price:1}).toArray(function(err, result) {
+    		if (err) throw err;
+    		for (i=0;i<result.length;i++){
+    			result[i].img="http://"+ip.address()+":"+constantm.port+result[i].img;
+    		}
+    		response.json(result);
+    		response.end();
+  			});
+  			db.close()
+		});
+});
+
+app.get('/product', function (request, response, next) {
+			var id=new ObjectID(request.query.id);
+			response.setHeader("Content-Type","application/json");
+			MongoClient.connect(murl, function(err, db) {  //get product details
+  			assert.equal(null, err);
+			db.collection("catalogitems").find({_id:id}).toArray(function(err, result) {
+    		if (err) throw err;
+    		for (i=0;i<result.length;i++){
+    			result[i].img="http://"+ip.address()+":"+constantm.port+result[i].img;
+    		}
+    		response.json(result);
+    		response.end();
+  			});  
+			db.close()
+		});  			
+
+});	
+
+
+app.listen(constantm.port)
