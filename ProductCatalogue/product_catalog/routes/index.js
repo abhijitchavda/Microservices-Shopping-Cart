abd8bdc93cbc473ce37c6208d218ac241f556f99
Abhijitsinh Chavda <abhijitsinh.chavda@sjsu.edu>
2017-11-03 15:53:34 -0700

    added api and render path for routing

--- ProductCatalogue/product_catalog/routes/index.js
+++ ProductCatalogue/product_catalog/routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/productcatalog', function(req, res, next) {
+  res.render('shop/index', { title: 'Express' });
 });
 
 module.exports = router;
