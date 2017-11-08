b0862c03cf62c320c2648069a1eb80d0b62d2e57
Abhijitsinh Chavda <abhijitsinh.chavda@sjsu.edu>
2017-11-07 21:36:52 -0800

    created route for /productcatalog api

--- StoreUI/ninja_grocery/app.js
+++ StoreUI/ninja_grocery/app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/productcatalog', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
