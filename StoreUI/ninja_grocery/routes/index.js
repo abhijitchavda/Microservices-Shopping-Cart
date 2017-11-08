2c425933005713ec1bd6d9c938880bf0b149f84d
Abhijitsinh Chavda <abhijitsinh.chavda@sjsu.edu>
2017-11-07 21:42:33 -0800

    created api call to backend with data parsing

--- StoreUI/ninja_grocery/routes/index.js
+++ StoreUI/ninja_grocery/routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
-
+var Request=require('request');
+var product;
+var catagori;
 /* GET home page. */
-router.get('/productcatalog', function(req, res, next) {
-  res.render('shop/index', { title: 'Express' });
+router.get('/', function(req, res, next) {
+	Request.get('http://10.0.0.183:8000/mostfav', function (error, response, body) {
+ 	           if (error) {
+                throw error;
+            }
+ 
+            data = JSON.parse(body);
+            product=data;
+            var productChunks=[];
+            var chunkSize=3;
+            for(var i=0;i< product.length;i +=chunkSize){
+            	productChunks.push(product.slice(i,i+chunkSize));
+            }
+            res.render('shop/index', { title: 'ninja product catalog',products: productChunks});
+});
+  
 });
 
 module.exports = router;
