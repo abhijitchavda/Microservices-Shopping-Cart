0d5cc241bd77d96bb93627a07da1fc040728c9f1
Neville Mascarenhas <nevosial@gmail.com>
2017-11-13 19:17:34 -0800

    simple http server and morgan

---
+++ Logging/morgan_usage/basic.js
@@ -0,0 +1,20 @@
+//Creating a basic nodejs server using core nodejs functions.
+//Using morgan middleware to logg the http requests.
+
+var finalhandler = require('finalhandler')
+var http = require('http')
+var morgan = require('morgan')
+
+// create "middleware"
+var logger = morgan('combined')
+
+http.createServer(function (req, res) {
+  var done = finalhandler(req, res)
+  logger(req, res, function (err) {
+    if (err) return done(err)
+
+    // respond to request
+    res.setHeader('content-type', 'text/plain')
+    res.end('hello, world!')
+  })
+})
