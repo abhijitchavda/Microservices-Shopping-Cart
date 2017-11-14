97a814b64556ae6768e986cd77a73e4a6ec1d195
Neville Mascarenhas <nevosial@gmail.com>
2017-11-13 23:26:04 -0800

    creation of log file using winston.

---
+++ Logging/winstonusage/wsampler.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const winston = require('winston');
+//const winston = require('./node_modules/lib/winston');
+
+const filename = path.join(__dirname, 'created-logfile.log');
+
+//
+// Remove the file, ignoring any errors
+//
+try { fs.unlinkSync(filename); }
+catch (ex) { }
+
+//
+// Create a new winston logger instance with two tranports: Console, and File
+//
+//
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename })
+  ]
+});
+
+logger.log('info', 'Hello created log files!', { 'foo': 'bar' });
+
+setTimeout(function () {
+  //
+  // Remove the file, ignoring any errors
+  //
+  //try { fs.unlinkSync(filename); }
+  //catch (ex) { }
+  console.log('inside setTimeout')
+}, 1000);
