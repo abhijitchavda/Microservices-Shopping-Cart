26a5fb7c07aa515b253f4dac1181ea8abf567e5a
Abhijitsinh Chavda <abhijitsinh.chavda@sjsu.edu>
2017-11-16 01:21:54 -0800

    export port

--- ProductCatalogue/api/nodejs/constants.js
+++ ProductCatalogue/api/nodejs/constants.js
@@ -1,4 +1,6 @@
 var limitvalue=2; //for home page to get top products based in avg ratings
 var ip="52.14.77.163";
+var port=3000;
 module.exports.limitvalue=limitvalue;
-modeule.exports.ip=ip;
+module.exports.ip=ip;
+module.exports.port=port;
