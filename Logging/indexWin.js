ebf598ce74126083dac9ddfc95ad02e5705faa1c
Neville Mascarenhas <nevosial@gmail.com>
2017-11-18 18:41:19 -0800

    Usable skeleton for the logger system

---
+++ Logging/indexWin.js
@@ -0,0 +1,43 @@
+const winston = require('winston');
+require('winston-mongodb').MongoDB;
+const fs = require('fs');
+const env = process.env.NODE_ENV || 'development';
+const logDir ='log';
+
+// Create the log directory if it does not exist
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+
+//const tsFormat = () => (new Date()).toLocaleTimeString();
+var date = new Date();
+const tsFormat = date;
+
+const logger = new (winston.Logger)({
+  transports: [
+    new(winston.transports.Console)({
+      timestamp: tsFormat,   //added timestamp
+      colorize: true,       // colorize.
+    }),
+    //Added a new transport to write logs to a file.
+    new (winston.transports.File)({
+      filename: `${logDir}/results.log`,
+      timestamp: tsFormat,
+      //level: env === 'development' ? 'debug' : 'info'  //log all 'info' messages.
+      level: env === 'development' ? 'debug' : 'verbose'  //log all messages.
+    }),
+    //Added a new transport to write log data to the logs collection in MongoDB.
+    new(winston.transports.MongoDB)({
+            db : 'mongodb://localhost:27017/logSystem',
+            collection: 'logs'
+    })
+  ]
+});
+
+//logger.level = 'debug';
+
+logger.info('Hello world');
+logger.debug('Debugging info');
+logger.verbose('Lots of data...');
+logger.warn('Warning message');
+logger.error('Error info');
