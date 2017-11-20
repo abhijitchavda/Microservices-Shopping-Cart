b963bd50e81c2cf11d2ef45152271fa3be96ee63
Niral Koradia <koradiya.niral@gmail.com>
2017-11-19 17:34:00 -0800

    Shopping Cart-- Remove Item
    Shopping Cart-- Remove Item API

--- ShoppingCart/shoping_cart/routes/server.js
+++ ShoppingCart/shoping_cart/routes/server.js
@@ -16,9 +16,9 @@ router.get('/ping', function(req, res, next) {
 
 router.get('/getallcartitem', function(req, res, next) {
     Cart.find()
-        .then(function(doc) {
+        .then(function(cartItems) {
             //res.render('index', {items: doc});
-            res.status(200).json({items: doc});
+            res.status(200).json({items: cartItems});
         });
 });
 
@@ -83,8 +83,8 @@ router.post('/additemtocart', function(req, res, next) {
 
                             var condition = {owner_user_id:req.body.owner_user_id};
                             var updateCol = {
-                                                $push : { products : newProduct},
-                                                $inc: { total_quantity: 1, total_price: req.body.products[0].product_price}
+                                $push : { products : newProduct},
+                                $inc: { total_quantity: 1, total_price: req.body.products[0].product_price}
                             };
 
                             Cart.update(condition, updateCol, {upsert: true}, function(err){
@@ -136,54 +136,107 @@ router.post('/additemtocart', function(req, res, next) {
     });
 });
 
+
 router.post('/removeitemfromcart', function(req, res, next) {
-    var id = req.body.id;
 
-    Admin.findById(id, function(err, doc) {
-        if (err) {
-            console.error('error, no entry found');
+    Cart.count({owner_user_id: req.body.owner_user_id}, function (err, count){
+        if(err)
+        {
+            console.log("Some Error Happened while matching owner ID");
+            return res.status(500).json({"Message": err});
         }
-        doc.firstname = req.body.fname;
-        doc.lastname = req.body.lname;
-        doc.save();
-    })
-    res.redirect('/');
-});
+        else {
+            if (count > 0) {
+                //document exists
+                console.log("Update cart");
+                Cart.count({ products: { $elemMatch : {product_id : req.body.products[0].product_id }}}, function (err, productCount) {
+                    if(err)
+                    {
+                        console.log("Some Error Happened while matching product ID");
+                        return res.status(500).json({"Message": err});
+                    }
+                    else
+                    {
+                        if(productCount>0)
+                        {
+                            //update product quantity
+                            var msg = "Same Product..reduce quantity";
+                            console.log(msg);
+                            Cart.count( {'products.product_quantity':1, 'products.product_id': req.body.products[0].product_id}, function (err, productArrCount) {
+                                if(err)
+                                {
+                                    console.log("Some Error Happened while counting product id");
+                                    return res.status(500).json({"Message": err});
+                                }
+                                else
+                                {
+                                    if(productArrCount==1)
+                                    {
+                                        //Remove product element
+                                        var msg = "Last Product..Remove";
+                                        console.log(msg);
 
+                                        var condition = {owner_user_id:req.body.owner_user_id};
+                                        var updateCol = {
+                                            $pull : { products : {product_id : req.body.products[0].product_id }},
+                                            $inc: { total_quantity: -1, total_price: -req.body.products[0].product_price}
+                                        };
 
-router.post('/addmembertocart', function(req, res, next) {
-    var id = req.body.id;
+                                        Cart.update(condition, updateCol, {upsert: true}, function(err){
+                                            if(err)
+                                            {
+                                                console.log("Some Error Happened while reomving last product");
+                                                return res.status(500).json({"Message": err});
+                                            }
+                                            else {
+                                                console.log("Quantity Updated");
+                                                return res.status(200).json({"Message": "update"});
+                                            }
+                                        });
+                                    }
 
-    Admin.findById(id, function(err, doc) {
-        if (err) {
-            console.error('error, no entry found');
+                                    else
+                                    {
+                                        //Reduce Quantity
+                                        var condition = {$and :
+                                            [
+                                                {owner_user_id:req.body.owner_user_id},
+                                                { products: { $elemMatch : {product_id : req.body.products[0].product_id }}}
+                                            ]};
+
+                                        var updateCol = {$inc: { "products.$.product_quantity": -1 , total_quantity: -1, total_price: -req.body.products[0].product_price} };
+                                        Cart.update(condition, updateCol, {upsert: true}, function(err){
+                                            if(err)
+                                            {
+                                                console.log("Some Error Happened while decrementing product quantity");
+                                                return res.status(500).json({"Message": err});
+                                            }
+                                            else {
+                                                console.log("Quantity Updated");
+                                                return res.status(200).json({"Message": "update"});
+                                            }
+                                        });
+                                    }
+                                }
+                            });
+                        }
+                        else
+                        {
+                            var msg = "Item doesn't exist..!!";
+                            console.log(msg);
+                            res.status(200).json({"Message": msg});
+                        }
+                    }
+                });
+            }
+            else {
+                var msg = "Cart doesn't exist..!!";
+                console.log(msg);
+                res.status(200).json({"Message": msg});
+            }
         }
-        doc.firstname = req.body.fname;
-        doc.lastname = req.body.lname;
-        doc.save();
-    })
-    res.redirect('/');
-});
-
-
-router.post('/removememberfromcart', function(req, res, next) {
-    var id = req.body.id;
-
-    Admin.findById(id, function(err, doc) {
-        if (err) {
-            console.error('error, no entry found');
-        }
-        doc.firstname = req.body.fname;
-        doc.lastname = req.body.lname;
-        doc.save();
     });
-    res.redirect('/');
 });
 
-router.post('/deletecart', function(req, res, next) {
-    var id = req.body.id;
-    Admin.findByIdAndRemove(id).exec();
-    res.redirect('/');
-});
 
 module.exports = router;
