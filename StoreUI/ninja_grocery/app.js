1836335dbcc7e8c1da40fca1815ecbff3049cdc7
prathmesh-parmar <31287314+prathmesh-parmar@users.noreply.github.com>
2017-11-26 21:25:30 -0800

    Update app.js

--- StoreUI/ninja_grocery/app.js
+++ StoreUI/ninja_grocery/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var winston = require('winston');
+require('winston-mongodb').MongoDB;
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var expressHbs = require('express-handlebars');
@@ -16,20 +18,49 @@ var checkout = require('./routes/checkout');
 var order = require('./routes/orders');
 var shoppingcart = require('./routes/shoppingcart');
 
+//User Sessions
+var session = require('express-session');
+var passport = require('passport');
+var validator = require('express-validator');
+var mongoStore = require('connect-mongo')(session);
+
 var app = express();
 
+var userRoutes = require('./routes/user')
+require('./config/passport');
 // view engine setup
 app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
-app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'views'));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validator());
 app.use(cookieParser());
+app.use(session({
+    secret: 'mysecret',
+    resave: false,
+    saveUninitialized: false,
+    store: new mongoStore({mongooseConnection: mongoose.connection}),
+    cookie: {maxAge: 10 * 60 * 1000}
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(flash());
+
+app.use(function (req, res, next) {
+    res.locals.login = req.isAuthenticated();
+    res.locals.session  = req.session;
+    console.log(res.locals.session);
+    next();
+});
+
+app.use('/user', userRoutes);
+app.use('/', routes);
 
 app.use('/productcatalog', productcatalog);
 app.use('/user', user);
@@ -43,9 +74,9 @@ app.use('/checkout/success', checkout);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -53,23 +84,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
