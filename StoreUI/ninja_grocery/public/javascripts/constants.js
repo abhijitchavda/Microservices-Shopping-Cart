6ee777a6ad1a06f28ea49d341a674c0376d8eb4d
niralkk <koradiya.niral@gmail.com>
2017-11-30 20:24:36 -0800

    AWS shopping Cart with load balancer

--- StoreUI/ninja_grocery/public/javascripts/constants.js
+++ StoreUI/ninja_grocery/public/javascripts/constants.js
@@ -1,7 +1,8 @@
 //var server_ip_pc="18.217.169.200";
 var server_ip_pc="product-catalog-server-load-1546340057.us-east-2.elb.amazonaws.com";
 var server_port_pc="3000";
-var server_ip_sc="13.56.155.199";
+//var server_ip_sc="13.56.155.199";
+var server_ip_sc="ninja-sc-load-108525937.us-west-1.elb.amazonaws.com";
 var server_port_sc="9000";
 var payment_api_endpoint = "http://54.193.66.242:5000/payment";
 var order_api_endpoint = "http://54.193.66.242:4000/order";
